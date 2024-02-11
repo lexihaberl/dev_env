6028bbe40b5d4f564326588b7283ae48734be806
Alexander Haberl <azteke42@gmail.com>
2024-02-11 21:35:29 +0100

    Update from docker-compose to docker compose

--- build_env.sh
+++ build_env.sh
@@ -1,4 +1,4 @@
 export DOCKER_UID="$(id -u)"
 export DOCKER_GID="$(id -g)"
-docker-compose -f docker-compose-base.yml build
-docker-compose build
+docker compose -f docker-compose-base.yml build
+docker compose build
